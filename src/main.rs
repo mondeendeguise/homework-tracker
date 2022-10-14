40bede2e84f5f1b9ff6f306e3f495233339216bd
A. Bullock <45837222+mondeendeguise@users.noreply.github.com>
2022-10-14 19:46:54 +0000

    refactoring in progress

--- src/main.rs
+++ src/main.rs
@@ -4,59 +4,6 @@ use time::PrimitiveDateTime;
 use time::macros::datetime;
 use url::{ Url, ParseError };
 
-#[derive(Debug)]
-pub struct Task {
-    id: usize,
-    pub title: String,
-    pub description: Option<String>,
-    pub url: Option<Result<Url, ParseError>>,
-    pub points: Option<u32>,
-    pub date_due: Option<PrimitiveDateTime>,
-    pub date_done: Option<PrimitiveDateTime>,
-    // priority: f64,
-    // late: bool,
-    // completed: bool,
-    // closed: bool,
-}
-
-impl Task {
-    pub fn new(
-        id: usize,
-        title: String,
-        description: Option<String>,
-        url: Option<Result<Url, ParseError>>,
-        points: Option<u32>,
-        date_due: Option<PrimitiveDateTime>,
-        date_done: Option<PrimitiveDateTime>,
-    ) -> Self {
-        Task {
-            id,
-            title,
-            description,
-            url,
-            points,
-            date_due,
-            date_done,
-            // priority: 0.0,
-            // late: false,
-            // completed: false,
-            // closed: false,
-        }
-    }
-
-    pub fn status() -> [bool; 3] {
-        // let current_time = std::time::SystemTime::now();
-        // I'm very frustrated as I cannot figure out getting
-        // the current date & time without needing to import
-        // another got damn crate >:(
-        todo!();
-    }
-
-    pub fn priority() -> f64 {
-        todo!();
-    }
-}
-
 fn main() {
     let mut tasks = Vec::new();
 
--- src/model/database.rs
+++ src/model/database.rs
@@ -1,5 +1,71 @@
-pub struct User {
-    pub id: u32,
-    pub name: String,
-    pub password: String,
+pub struct Grade {
+    pub points: u32,
+    pub score: u32,
+}
+
+impl Grade {
+    pub fn grade() -> f64 {
+        (self.score / self.points)
+    }
+}
+
+#[derive(Debug)]
+pub struct Task {
+    pub id: usize,
+    pub title: String,
+    pub description: Option<String>,
+    pub url: Option<Result<Url, ParseError>>,
+    pub grade: Option<Grade>,
+    pub date_due: Option<PrimitiveDateTime>,
+    pub date_done: Option<PrimitiveDateTime>,
+}
+
+
+impl Task {
+    pub fn new(id: usize, title: String, description: Option<String>, url: Option<Result<Url, ParseError>>, grade: Option<Grade>, date_due: Option<PrimitiveDateTime>, date_done: Option<PrimitiveDateTime>) -> Self {
+        Self {
+            id,
+            title,
+            description,
+            url,
+            points,
+            date_due,
+            date_done,
+        }
+    }
+
+    pub fn status() -> [bool; 3] {
+        // let current_time = std::time::SystemTime::now();
+        // I'm very frustrated as I cannot figure out getting
+        // the current date & time without needing to import
+        // another got damn crate >:(
+        todo!();
+    }
+}
+
+pub struct Group {
+    pub id: usize,
+    pub title: String,
+    pub description: Option<String>,
+    pub url: Option<Result<Url, ParseError>>,
+    pub items: Vec<Task>,
+}
+
+impl Group {
+    pub fn new(id: usize, title: String, description: Option<String>, url: Option<Result<Url, ParseError>>) -> Self {
+        Self {
+            id,
+            title,
+            description,
+            url,
+            Vec::new(),
+        }
+    }
+
+    pub fn total_points() -> u32 {
+        for i in 
+    }
+    pub fn priority(id: usize) -> f64 {
+        todo!()
+    }
 }
